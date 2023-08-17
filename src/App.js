be6bd3fed12a1be62ce53a7a834aa7b8d0734fb0
Dillon Dickerson <dickerson.dillon@gmail.com>
2023-08-17 17:41:37 -0400

    Installed React-Browser-Router and added pages folder

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import Header from './components/Header';
 import Footer from './components/Footer';
 import Nav from './components/Nav';
+import {routes, route} from "react-router-dom";
 
 function App() {
   return (
@@ -21,5 +22,6 @@ function App() {
     </>
   );
 }
+
 
 export default App;

70aae4967ee72f4577e6286004c4f886b183ac55
Dillon Dickerson <dickerson.dillon@gmail.com>
2023-08-21 16:47:48 -0400

    completed description component with information about restaurant

--- src/components/Description.jsx
+++ src/components/Description.jsx
@@ -1,8 +1,19 @@
-import React from 'react'
+import React from 'react';
+import restaurantfood from '../assets/restauranfood.jpg';
 
 const Description = () => {
   return (
-    <div>Description</div>
+    <>
+      <div className="restaurant-desc-parent">
+            <div className="restaurant-desc">
+                <p className="title desc-title">Little Lemon</p>
+                <p className="subtitle desc-subtitle">Chicago</p>
+                <p className="desc-text desc">We are a family owned Mediterranean restaurant, 
+                    focused on traditional recipes served with a modern twist.</p>
+            </div>
+            <img className="desc-img" src={restaurantfood} alt="hero image"/>
+        </div>
+    </>
   )
 }
 

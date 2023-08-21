161939f8d3bea8256a30d128c7d8e632908e4298
Dillon Dickerson <dickerson.dillon@gmail.com>
2023-08-21 14:15:53 -0400

    Completed navigation menu and mobile navigation logic

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,17 +1,29 @@
-import React from 'react'
+import React from 'react';
+import { Link} from 'react-router-dom';
+import {useState} from 'react';
+import {FaRegTimesCircle, FaGripLines} from 'react-icons/fa';
 
 const Nav = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleNav = () => {
+    setIsOpen(!isOpen);
+  };
+
   return (
-    <nav>
-      <ul>
-        <a><li>Home</li></a>
-        <a><li>About</li></a>
-        <a><li>Menu</li></a>
-        <a><li>Reservations</li></a>
-        <a><li>Order Online</li></a>
-        <a><li>Login</li></a>
-      </ul>
-    </nav>
+  <>
+      <div className="mobile-nav" onClick={toggleNav}>
+        {isOpen ? <FaRegTimesCircle /> : <FaGripLines />}
+      </div>
+      <nav className={`nav ${isOpen ? 'active' : ''}`} onClick={toggleNav}>
+      <Link to="/" className="nav-item">HOME</Link>
+        <a href="#" className="nav-item">ABOUT</a>
+        <a href="#" className="nav-item">MENU</a>
+        <Link to="/booking" className="nav-item">RESERVATIONS</Link>
+        <a href="#" className="nav-item">ORDER ONLINE</a>
+        <a href="#" className="nav-item">LOGIN</a>
+      </nav>
+  </>
   )
 }
 
